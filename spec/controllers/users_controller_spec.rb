722304f78594f88328f42c13d99da29c31600a5f
Oleksandr Baboshyn <oleksandr@iMac-Oleksandr.local>
2017-12-05 12:06:18 +0200

    tests for users_controller added

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  let(:attrs) { attributes_for(:user) }
+  let(:user) { instance_double(User, id: 1, as_json: attrs, **attrs) }
+
+
+  describe '#create' do
+    let(:resource_params) { attributes_for(:user) }
+
+    before do
+      allow(UsersCreator).to receive(:new).with(permit!(resource_params)) do
+        double.tap { |users_creator| allow(users_creator).to receive(:create).and_return(user) }
+      end
+    end
+
+    context '#parameters for qanswer passed validation'do
+      before { allow(user).to receive(:valid?).and_return(true) }
+
+      before { process :create, method: :post, params: { user: resource_params }, format: :json }
+
+      it { expect(response.body).to eq user.to_json }
+
+      it { expect(response).to have_http_status 201 }
+    end
+
+    context '#parameters for answer did not pass validation'do
+      let(:errors) { instance_double(ActiveModel::Errors) }
+
+      before { allow(user).to receive(:valid?).and_return(false) }
+
+      before { allow(user).to receive(:errors).and_return(errors) }
+
+      before { process :create, method: :post, params: { user: resource_params }, format: :json }
+
+      it { expect(response.body).to eq errors.to_json }
+
+      it { expect(response).to have_http_status 422 }
+    end
+  end
+end
