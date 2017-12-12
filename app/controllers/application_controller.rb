b3d5eaab91961133b5b129b1e230bc5a30b5ca08
Oleksandr Baboshyn <oleksandr@iMac-Oleksandr.local>
2017-12-12 14:21:45 +0200

    before_atction replaced

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
   include Authenticatable
-  before_action :authenticate
+  # before_action :authenticate
 
   rescue_from ActiveRecord::RecordNotFound do
     head 404
--- app/services/authenticatable.rb
+++ app/services/authenticatable.rb
@@ -1,6 +1,10 @@
 module Authenticatable
   attr_reader :current_user
 
+  def self.included(base)
+    base.before_action :authenticate
+  end
+
   def authenticate
     authenticate_or_request_with_http_token do |token|
 
