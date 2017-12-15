4c044e0854835866d207d1a017945c43febbe0b1
Alexbaboshyn <baboshyn@mlsdev.com>
2017-12-15 17:20:48 +0200

    peofiles_controller_spec added

---
+++ spec/controllers/profiles_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe ProfilesController, type: :controller do
+  it { is_expected.to be_kind_of(Authenticatable) }
+
+  it { is_expected.to be_an ApplicationController }
+
+  let(:user) { instance_double User }
+
+  before { sign_in user }
+
+  describe '#show' do
+    before { process :show, method: :get, format: :json }
+
+    it { expect(response).to have_http_status 200 }
+
+    it { expect(response.body).to eq user.to_json }
+  end
+end
