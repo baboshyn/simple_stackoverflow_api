9471ec1aa8f0c18ff56a5bdbb5b9617032dc2f79
Alexbaboshyn <baboshyn@mlsdev.com>
2017-12-29 16:46:16 +0200

    some files removed from public

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe 'Authentication', type: :request do
 
     it('returns HTTP Status Code 401') { expect(response).to have_http_status 401 }
 
-    it('returns "HTTP Token: Access denied.') { expect(response.body).to eq "HTTP Token: Access denied.\n" }
+    it('returns "HTTP Token: Access denied."') { expect(response.body).to eq "HTTP Token: Access denied.\n" }
 
     it('returns header "WWW-Authenticate"') { expect(response.header['WWW-Authenticate']).to eq "Token realm=\"Application\"" }
   end
