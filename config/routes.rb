eae58cd88a8fb6aab41f81f23226022cedacdfb3
Alexbaboshyn <baboshyn@mlsdev.com>
2018-01-23 14:38:08 +0200

    order of actions changed in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :questions, only: [:show, :index, :create, :update, :destroy]
+  resources :questions, only: [:index, :show, :create, :update, :destroy]
 
-  resources :answers, only: [:index, :update, :create, :destroy]
+  resources :answers, only: [:index, :create, :update, :destroy]
 
   resources :users, only: :create do
     collection do
