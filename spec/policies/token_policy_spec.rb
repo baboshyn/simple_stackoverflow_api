9458600ebb24bf2b034d53a81eb8ca92821904c0
Alexbaboshyn <baboshyn@mlsdev.com>
2018-01-26 16:17:14 +0200

    token policy refactored

--- spec/policies/token_policy_spec.rb
+++ spec/policies/token_policy_spec.rb
@@ -3,17 +3,13 @@ require 'rails_helper'
 describe TokenPolicy do
   subject { described_class }
 
-  let(:confirmed_user) { User.new(state: :confirmed) }
-
-  let(:unconfirmed_user) { User.new }
-
   permissions :create? do
     it 'refuses access if user is unconfirmed' do
-      expect(subject).not_to permit(unconfirmed_user, :Token)
+      expect(subject).not_to permit(User.new, :Token)
     end
 
     it 'grants access if user is confirmed' do
-      expect(subject).to permit(confirmed_user, :Token)
+      expect(subject).to permit(User.new(state: :confirmed), :Token)
     end
   end
 end
